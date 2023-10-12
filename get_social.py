401b9d4e01ac9c553518c83b159fab30799416b2
chelseaw2525 <chelseawen03@gmail.com>
2023-10-12 09:08:28 -0700

    update1

--- get_social.py
+++ get_social.py
@@ -10,27 +10,18 @@ from selenium.webdriver.support import expected_conditions as EC
 import traceback
 import re
 
-def find_this(driver, partial_link_text):
-    xpath_expression = "//a[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '{}')]".format(partial_link_text.lower())
-    elements = driver.find_elements(By.XPATH, xpath_expression)
-    return elements
-
 scopes = ['https://www.googleapis.com/auth/drive']
 creds = Credentials.from_service_account_file("C:\\Users\\bookw\\Downloads\\liquid-receiver-386602-04d443fa11c8.json")
 creds = creds.with_scopes(scopes)
 client = gspread.authorize(credentials=creds)
 
 driver = webdriver.Chrome()
-
 sheet_name = "0 - Blockchain Data"
 sheet = client.open(sheet_name)
 worksheet = sheet.get_worksheet(0)
-wait = WebDriverWait(driver, 5)
+wait = WebDriverWait(driver, 1)
 
 print("can see sheet")
-
-#def update_worksheet_cell(cell, value):
-#    worksheet.update(cell, value)
 
 def update_worksheet_cell(cell, result):
     target = worksheet.acell(cell).value
@@ -38,7 +29,7 @@ def update_worksheet_cell(cell, result):
         worksheet.update(cell, result)  # Update cell at column with result
 
 
-for row in range(577, 3085):
+for row in range(847, 3085):
 
     link = worksheet.acell(f"{chr(ord('D'))}{row}").value
     title = worksheet.acell(f"{chr(ord('A'))}{row}").value
@@ -67,7 +58,7 @@ for row in range(577, 3085):
             discord = discord_links[0]
             update_worksheet_cell(f"{chr(ord('G'))}{row}",discord)
         
-        print("checked for socials")
+        print("updated socials")
         time.sleep(2)
 
     except Exception as e:
