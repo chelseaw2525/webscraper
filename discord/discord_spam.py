e640276032e8a75a0df3c5c7dc2353f156693965
chelseaw2525 <chelseawen03@gmail.com>
2023-12-05 22:14:35 -0800

    bunch of cleanup and making things more readable

--- discord/discord_spam.py
+++ discord/discord_spam.py
@@ -5,10 +5,10 @@ import time
 
 driver = webdriver.Chrome()
 driver.get("https://discordapp.com/login")
-time.sleep(30)
+time.sleep(30) #enter login info and navigate to target manually
 for i in range (0, 10000):
 	try:
-		element = driver.find_element(By.CSS_SELECTOR, "[aria-label='Message @andrewhc']")
+		element = driver.find_element(By.CSS_SELECTOR, "[aria-label='Message #off-topic']")
 		element.click()
 		element.send_keys('get spammed lol')
 		element.send_keys(Keys.ENTER)
