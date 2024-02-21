a08f1c204457e4206fe62bc1babd56221685acfc
chelsea <chelseawen03@gmail.com>
2024-02-21 01:41:13 -0800

    cleanup and added time measure

--- rustscraper/src/sheets.rs
+++ rustscraper/src/sheets.rs
@@ -1,20 +1,18 @@
 extern crate google_sheets4 as sheets4;
-use sheets4::api::ValueRange;
-use sheets4::{Result, Error};
-use std::default::Default;
-use sheets4::{Sheets, oauth2, hyper, hyper_rustls, chrono, FieldMask};
-
+use sheets4::Error;
+use sheets4::{Sheets, oauth2, hyper, hyper_rustls};
+use std::time::*;
 
 #[tokio::main]
 async fn main() {
-    
-    let sheet_id = "14s1FeqI--U8w0fdcCfeKOqSgwf59ZYwBaJUilSLSnSI";
+    let now = Instant::now();
+    let sheet_id = "1Sizuz41uvd03jt7mFtJ7oPrPPcHAkGJqUxunz_R1iog";
 
     let secret= oauth2::read_service_account_key("C:\\Users\\bookw\\Downloads\\liquid-receiver.json").await.expect("key not found");
     let client = hyper::Client::builder().build(hyper_rustls::HttpsConnectorBuilder::new().with_native_roots().https_or_http().enable_http1().enable_http2().build());
     let auth = oauth2::ServiceAccountAuthenticator::with_client(secret, client.clone()).build().await.expect("auth failed");
     let hub = Sheets::new(client.clone(), auth);
-    let result = hub.spreadsheets().values_get(sheet_id, "A1:A5").doit().await;
+    let result = hub.spreadsheets().values_get(sheet_id, "database!A2:A3421").doit().await;
     
     match result {
         Err(e) => match e {
@@ -31,6 +29,9 @@ async fn main() {
             |Error::FieldClash(_)
             |Error::JsonDecodeError(_, _) => println!("{}", e),
         },
-        Ok(res) => println!("Success: {:?}", res),
+        Ok((_, data)) => println!("results: {:?}", data.values.unwrap()),
     }
+    let elapsed_time = now.elapsed();
+    println!("Time @ {} microsec.", elapsed_time.as_micros());
+    
 }
